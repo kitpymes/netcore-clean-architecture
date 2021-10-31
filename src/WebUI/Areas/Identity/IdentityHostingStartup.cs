2da21f27a2c4ccdbd7a39a31d9d2740523496393
sferrari <sferrari.net@gmail.com>
2021-10-31 14:38:00 +0100

    Add project files.

---
+++ src/WebUI/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using CleanArchitecture.Infrastructure.Identity;
+using CleanArchitecture.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CleanArchitecture.WebUI.Areas.Identity.IdentityHostingStartup))]
+namespace CleanArchitecture.WebUI.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                //services
+                //    .AddIdentity<ApplicationUser, IdentityRole>(opt =>
+                //    {
+                //        opt.Password.RequiredLength = 6;
+                //        opt.Password.RequireDigit = false;
+                //        opt.Password.RequireUppercase = false;
+
+                //        opt.User.RequireUniqueEmail = true;
+
+                //        opt.SignIn.RequireConfirmedEmail = true;
+                //    })
+                //    .AddEntityFrameworkStores<ApplicationDbContext>()
+                //    .AddDefaultTokenProviders();
+
+                //services
+                //    .AddDefaultIdentity<ApplicationUser>(opt =>
+                //    {
+                //        opt.Password.RequiredLength = 6;
+                //        opt.Password.RequireDigit = false;
+                //        opt.Password.RequireUppercase = false;
+
+                //        opt.User.RequireUniqueEmail = true;
+
+                //        opt.SignIn.RequireConfirmedEmail = true;
+                //    })
+                //    .AddRoles<IdentityRole>();
+                    //.AddEntityFrameworkStores<ApplicationDbContext>();
+            });
+        }
+    }
+}
